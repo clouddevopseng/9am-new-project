d49ac183055a4b2d6b93356f1b95799fecdcbad0
john <john@tcs.com>
2025-10-27 09:47:09 +0530

    maven with springboot applition

---
+++ src/main/java/_am_new_project/_am_new_project/Application.java
@@ -0,0 +1,13 @@
+package _am_new_project._am_new_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/_am_new_project/_am_new_project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package _am_new_project._am_new_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
